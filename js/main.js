3f8ba4454b1d71aa164585d5aa5ad8ad802b8b07
Vasiliy Bogoyavlenskiy <mr.d.slack@gmail.com>
2014-06-23 18:42:16 +0400

    item animation 0.2

--- js/main.js
+++ js/main.js
@@ -1,19 +1,18 @@
-// $(document).ready(function(){
-// 	$('.pop-new-item').on('mouseenter', function(){
-// 		$(this).find('.item-header').fadeOut('fast');
-// 			$(this).find('.item-annotation').animate({'height':'100%'},'fast', function(){
-// 				$(this).find('.item-description-wrap').css('display','table').fadeIn();
-// 				});
+$(document).ready(function(){
+	$('.pop-new-item').on('mouseenter', function(){
+			$(this).find('.item-annotation').animate({'height':'100%'},'fast', function(){
+				$(this).find('.item-description-wrap').fadeIn('fast').css('display','table');
+				});
 			
-// 		}); 
+		}); 
 	
-// 	$('.pop-new-item').on('mouseleave', function(){
-// 		$(this).find('.item-description-wrap').css('display','none');
-// 		$(this).find('.item-annotation').animate({'height':'40px'}, 'fast', function(){
-// 			$(this).find('.item-header').fadeIn('fast');
-// 		}); 
-// 	});	
-// });
+	$('.pop-new-item').on('mouseleave', function(){
+		$(this).find('.item-description-wrap').css('display','none');
+		$(this).find('.item-annotation').animate({'height':'40px'}, 'fast', function(){
+			$(this).find('.item-header').fadeIn('fast');
+		}); 
+	});	
+});
 
 
 
