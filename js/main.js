0e864ece14222cfe8424a5aede4548e960469dad
slack <slack@slack.ru>
2014-06-23 23:25:48 +0400

    animation complete

--- js/main.js
+++ js/main.js
@@ -1,16 +1,12 @@
 $(document).ready(function(){
 	$('.pop-new-item').on('mouseenter', function(){
-			$(this).find('.item-annotation').animate({'height':'100%'},'fast', function(){
-				$(this).find('.item-description-wrap').fadeIn('fast').css('display','table');
+			$(this).find('.item-annotation').stop().animate({'height':'100%'},'fast', function(){
+				$(this).find('.item-description-wrap').stop().fadeIn('fast').css('display','table');
 				});
-			
 		}); 
-	
 	$('.pop-new-item').on('mouseleave', function(){
 		$(this).find('.item-description-wrap').css('display','none');
-		$(this).find('.item-annotation').animate({'height':'40px'}, 'fast', function(){
-			$(this).find('.item-header').fadeIn('fast');
-		}); 
+		$(this).find('.item-annotation').stop().animate({'height':'40px'}, 'fast'); 
 	});	
 });
 
