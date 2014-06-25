8a0cf2e205ff5af6cd52c27c7b279c7cc637d089
Vasiliy Bogoyavlenskiy <mr.d.slack@gmail.com>
2014-06-25 19:00:43 +0400

    add carousel

--- js/main.js
+++ js/main.js
@@ -8,6 +8,41 @@ $(document).ready(function(){
 		$(this).find('.item-description-wrap').css('display','none');
 		$(this).find('.item-annotation').stop().animate({'height':'40px'}, 'fast'); 
 	});	
+
+	
+	$('#first-slide-btn').on('click',function(){
+		$('#second-slide-btn').removeClass('active');
+		$('#third-slide-btn').removeClass('active');
+		$(this).addClass('active');
+		
+		$('#first-slide').fadeIn();
+		$('#second-slide').addClass('next').fadeOut('slow',function(){$(this).removeClass('next')});
+		$('#third-slide').addClass('next').fadeOut('slow',function(){$(this).removeClass('next')});
+		 
+	});
+
+	$('#second-slide-btn').on('click',function(){
+		$('#first-slide-btn').removeClass('active');
+		$('#third-slide-btn').removeClass('active');
+		$(this).addClass('active');
+
+		$('#second-slide').fadeIn();
+		$('#first-slide').addClass('next').fadeOut('slow',function(){$(this).removeClass('next')});
+		$('#third-slide').addClass('next').fadeOut('slow',function(){$(this).removeClass('next')});
+		 
+	});
+
+	$('#third-slide-btn').on('click',function(){
+		$('#first-slide-btn').removeClass('active');
+		$('#second-slide-btn').removeClass('active');
+		$(this).addClass('active');
+
+		$('#third-slide').fadeIn(); 
+		$('#first-slide').addClass('next').fadeOut('slow',function(){$(this).removeClass('next')});
+		$('#second-slide').addClass('next').fadeOut('slow',function(){$(this).removeClass('next')});
+		
+	});
+
 });
 
 
