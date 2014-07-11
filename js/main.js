4a708f71402816ba6f59da8548b1199c2f1484e8
Vasiliy Bogoyavlenskiy <mr.d.slack@gmail.com>
2014-07-11 18:40:28 +0400

    sticker alpha 0.1

--- js/main.js
+++ js/main.js
@@ -9,41 +9,6 @@ $(document).ready(function(){
 		$(this).find('.item-description-wrap').css('display','none');
 		$(this).find('.item-annotation').stop().animate({'height':'40px'}, 'fast'); 
 	});	
-
-	
-	// $('#first-slide-btn').on('click',function(){
-
-	// 	$('#second-slide-btn').removeClass('active');
-	// 	$('#third-slide-btn').removeClass('active');
-	// 	$(this).addClass('active');
-		
-	// 	$('#first-slide').fadeIn('slow');
-	// 	$('#second-slide').addClass('next').fadeOut(400,function(){$(this).removeClass('next')});
-	// 	$('#third-slide').addClass('next').fadeOut(400,function(){$(this).removeClass('next')});
-		 
-	// });
-
-	// $('#second-slide-btn').on('click',function(){
- // 		$('#first-slide-btn').removeClass('active');
- // 		$('#third-slide-btn').removeClass('active');
- // 		$(this).addClass('active');
- 
- // 		$('#second-slide').fadeIn('slow');
- // 		$('#first-slide').addClass('next').fadeOut(400,function(){$(this).removeClass('next')});
- // 		$('#third-slide').addClass('next').fadeOut(400,function(){$(this).removeClass('next')});
- 		 
- // 	});
-
-	// $('#third-slide-btn').on('click',function(){
-	// 	$('#first-slide-btn').removeClass('active');
-	// 	$('#second-slide-btn').removeClass('active');
-	// 	$(this).addClass('active');
-
-	// 	$('#third-slide').fadeIn('slow'); 
-	// 	$('#first-slide').addClass('next').fadeOut(400,function(){$(this).removeClass('next')});
-	// 	$('#second-slide').addClass('next').fadeOut(400,function(){$(this).removeClass('next')});
-		
-	// });
 	$('.slide-btn').on('click',function(){
 		$('.slide-btn').removeClass('active');
 		$(this).addClass('active');
